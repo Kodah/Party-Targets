342ae56fd5b3f6f77b0fda6c23218e5b20e126be
tom <tom_1440@hotmail>
2017-07-23 02:19:13 +0100

    Fix battle grounds and add extra slash commands for size

--- PartyTargets.lua
+++ PartyTargets.lua
@@ -2,6 +2,7 @@ local PT = CreateFrame("Frame", PT, UIParent);
 local anchors = {}
 
 local InArena = function() return (select(2,IsInInstance()) == "arena") end
+local InBG = function() return (select(2,IsInInstance()) == "PVP") end
 
 function PT:HideAll() 
     for _, anchor in ipairs(anchors) do 
@@ -14,17 +15,16 @@ function PT:CreateAnchors()
 	for i=0,GetNumGroupMembers()  do
         if not anchors[i] then 
             local anchor = CreateFrame("Frame","PTAnchor"..i ,UIParent)
-            anchor:SetWidth(200);
+            anchor:SetWidth(350);
             anchor:SetHeight(20);
             anchor:SetPoint("Center", UIParent, 0, 0);
             
             anchor.myIndex = i
-
             anchor.text = anchor:CreateFontString()
-            anchor.text:SetFont("Fonts\\FRIZQT__.TTF", 15, "OUTLINE")
+            anchor.text:SetFont("Fonts\\FRIZQT__.TTF", SETTINGS.textSize, "OUTLINE")
             anchor.text:SetText(i)
             anchor.text:SetPoint("CENTER", anchor, "CENTER",0, 0)
-            anchor.text:SetWidth(200);
+            anchor.text:SetWidth(350);
             anchor.text:SetHeight(20);
             anchor:Hide();
             anchors[i] = anchor
@@ -38,7 +38,7 @@ function PT:LoadPositions()
 	for k,anchor in ipairs(anchors) do
 		if (GetNumGroupMembers() >= k)  then
 			anchor:ClearAllPoints()
-			if (raidFramesOn == 1) or (UnitInRaid("player")) and not InArena() then
+			if (raidFramesOn == 1) or (UnitInRaid("player")) and (not InArena() or not InBG()) then
                 local raidFrame = nil
                 if CompactRaidFrameManager_GetSetting("KeepGroupsTogether") then
                     if UnitInRaid("player") then
@@ -66,9 +66,21 @@ end
 function PT:UpdateFrame(anchor, frame) 
     local target = frame.unit.."-target"
     if not SETTINGS.showHealers and UnitGroupRolesAssigned(frame.unit) == "HEALER" then return end 
-    anchor.text:SetText(UnitName(target));
+
+    anchor.text:SetFont("Fonts\\FRIZQT__.TTF", SETTINGS.textSize, "OUTLINE")
+    anchor.text:SetText(_G[SETTINGS.displayFunction](target));
     anchor.text:SetJustifyH(SETTINGS.textAlignment);
     anchor:SetPoint(SETTINGS.alignment1, frame, SETTINGS.alignment2)
+
+    if SETTINGS.alignment1 == "CENTER" then 
+        local width, _ = frame:GetSize()
+        anchor.text:SetWidth(width)
+        anchor:SetWidth(width)
+    else 
+        anchor.text:SetWidth(350)
+        anchor:SetWidth(350)
+    end 
+
     _, r = UnitClass(target);
     colour1 = RAID_CLASS_COLORS[r]
     if colour1 then 
@@ -108,9 +120,10 @@ PT:SetScript("OnEvent", function(self, event, arg1)
             SETTINGS.alignment2 = "CENTER"
             SETTINGS.textAlignment = "CENTER"
             SETTINGS.showHealers = true
-            
+            SETTINGS.displayFunction = "UnitName"
+            SETTINGS.textSize = 15
         end
-
+        SETTINGS.textSize = 15
     elseif event == "PLAYER_LOGOUT" then
         -- Save the time at which the character logs out
         -- HaveWeMetLastSeen = time()
@@ -147,14 +160,37 @@ function SlashCmdList.PT(msg)
             print("PartyTargets wont show healer targets")
         end
         PT:LoadPositions()
-    
+
+    elseif msg == "name" then 
+        SETTINGS.displayFunction = "UnitName"
+        print("PartyTargets will show targets names ")
+        PT:LoadPositions()
+    elseif msg == "class" then 
+        SETTINGS.displayFunction = "UnitClass"
+        print("PartyTargets will show targets class")
+        PT:LoadPositions()
+    elseif msg == "small" then
+        SETTINGS.textSize = 10
+        print("PartyTargets text is small")
+        PT:LoadPositions()
+    elseif msg == "medium" then
+        SETTINGS.textSize = 15
+        print("PartyTargets text is medium")
+        PT:LoadPositions()
+    elseif msg == "large" then 
+        SETTINGS.textSize = 20
+        print("PartyTargets text is large")
+        PT:LoadPositions()
+
     else 
         print("-- Party Targets --")
         print("Command list :")
         print("'pt left' - Align to left")
         print("'pt center' - Align to center")
         print("'pt right' - Align to right")
-        print("'pt healer' will toggle displaying healers targets")
+        print("'pt healer' Will toggle displaying healers targets")
+        print("'pt class' Shows target class instead of name")
+        print("'pt name' Shows target name instead of class")
     end
     
 end
